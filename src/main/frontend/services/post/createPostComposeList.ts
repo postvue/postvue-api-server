06bf68867aab1d8acc72462b9b3f4a021e1cabd9
Changmin Kim <ckd29672@naver.com>
2025-03-18 13:20:31 +0900

    fix: 오류 수정

--- src/main/frontend/services/post/createPostComposeList.ts
+++ src/main/frontend/services/post/createPostComposeList.ts
@@ -3,10 +3,11 @@ import {ADMIN_POST_COMPOSE_LIST_API_PATH} from "Frontend/services/appApiPath";
 import axios from "axios";
 
 export const createPostCompose = (formData: FormData): Promise<boolean> => {
+    console.log(formDate);
     return formApi
         .post(`${ADMIN_POST_COMPOSE_LIST_API_PATH}`, formData)
         .then((res) => {
-
+            console.log(data)
             const data:boolean = res.data.data;
             return data;
         })
